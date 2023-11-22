8e58ba67c1b3b93fff9407bfcb512a0d2307c778
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-11-22 20:22:05 +0100

    falta poco

--- EventoMania/EventoMania/src/app/app.component.ts
+++ EventoMania/EventoMania/src/app/app.component.ts
@@ -10,21 +10,27 @@ import data from '../assets/data.json';
 })
 export class AppComponent implements OnInit {
   eventos: Evento[] = [];
+
   fechaactual: Date;
-  directions: string[] =[];
-  selected_direction: string ='';
-  filtrated_directions: Evento[] =[];
+
+  uniquedirectios: string[] =[];
+
+  selecteddirections: string ='';
+
+  filtrateddirections: Evento[] =[];
 
   ngOnInit() {
     // Cargamos el fichero JSON
     const json: any = data;
+    this.filtrateddirections;
+
     // Guardamos el fichero cargado en el array de Eventos
     this.eventos = json;
+
     // Convertimos las fechas a tipo Date
     this.eventos.map((value) => value.fecha = new Date(value.fecha));
+
     this.fechaactual = new Date();
-    this.directions = this.getdirections();
-    this.soltarPorDireccion();
   }
 proximosEventos(): Evento[]{
   return this.eventos.filter(Evento => Evento.fecha >= this.fechaactual);
@@ -32,18 +38,5 @@ proximosEventos(): Evento[]{
 
 Eventospasados(): Evento[]{
   return this.eventos.filter(Evento => Evento.fecha <= this.fechaactual);
-}
-
-getdirections(): string[]{
-const direcionesSet = new Set<string>();
-this.eventos.forEach(evento =>direcionesSet.add(evento.direccion));
-return Array.from(direcionesSet);
-
-}
-soltarPorDireccion(){
-this.filtrated_directions = this.eventos.filter(Evento => Evento.direccion === this.selected_direction);
-}
-ondiretion(){
-  this.soltarPorDireccion();
 }
  }
